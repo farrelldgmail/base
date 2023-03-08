261460f0ac6d47f8b7f827423fcfefc44b49d02d
Type-D <farrelld@gmail.com>
2023-03-08 14:11:21 -0500

    Add project to git repo

---
+++ src/views/Login.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-form v-model="valid">
+        <v-container>
+            <v-row>
+                <v-col
+                    cols="12"
+                ><h2>Login page</h2>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col
+                    cols="12"
+                    md="6"
+                >
+                <v-text-field
+                    v-model.trim="firstname"
+                    :rules="nameRules"
+                    label="First name"
+                    required
+                ></v-text-field>                
+                </v-col>
+                <v-col
+                    cols="12"
+                    md="6"
+                >
+                <v-text-field
+                    v-model.trim="lastname"
+                    :rules="nameRules"
+                    label="Last name"
+                    required
+                ></v-text-field>                   
+                </v-col>
+            </v-row>
+
+            <v-row>
+                <v-col
+                    cols="12"
+                    md="6"
+                ><v-btn to="../">Go back home</v-btn>
+                </v-col>
+                <v-col
+                    cols="12"
+                    md="6"
+                ><v-btn :disabled="!valid" type="submit" >Login</v-btn>
+                </v-col>
+            </v-row>            
+    
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      valid: false,
+      firstname: '',
+      lastname: '',
+      nameRules: [
+        value => {
+          if (value) return true
+
+          return 'Name is requred.'
+        },
+      ],
+    }),
+  }
+</script>
